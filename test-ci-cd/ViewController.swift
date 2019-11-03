2fc12300a2823bfb94b7baf9e2b00e2aeafd7b6f
Ibrahim Zakarya <ibrahim@tradinos.com>
2019-11-03 10:08:38 +0200

    Initial Commit

---
+++ test-ci-cd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-ci-cd
+//
+//  Created by Ibrahim Zakarya on 11/3/19.
+//  Copyright © 2019 Tradinos UG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test-ci-cdTests/test_ci_cdTests.swift
@@ -0,0 +1,34 @@
+//
+//  test_ci_cdTests.swift
+//  test-ci-cdTests
+//
+//  Created by Ibrahim Zakarya on 11/3/19.
+//  Copyright © 2019 Tradinos UG. All rights reserved.
+//
+
+import XCTest
+@testable import test_ci_cd
+
+class test_ci_cdTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
